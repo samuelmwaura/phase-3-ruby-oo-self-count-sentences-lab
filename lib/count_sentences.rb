fba520e7b4ca4934a81d73932b3ccbe467d57556
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-19 01:12:29 +0300

    Completed the lab.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
 require 'pry'
 
+
+#We are monkey patching - altering the ruby inbuilt class String to include a custom inbuilt instance method.
 class String
 
   def sentence?
-    
+    if self.end_with? '.'
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
+   #Split the given method to an array on .!?
+   #Reject out any empty strings that may be included
+   #count the number of elements n th resultant array
 
+    self.split(/[.!?]+/).reject(&:empty?).count #Means you are dividing this into individual sentences at the specified symbols
   end
 end
+
+
+pp "This is not home?It is where I desire to be at.".count_sentences
